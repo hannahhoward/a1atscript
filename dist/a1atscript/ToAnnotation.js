00daceaeee6a352ab53bcf5e19baa25f52db8394
Hannah Howard <hannah@hannahhoward.net>
2015-04-28 12:02:07 -0700

    Add babel preprocessor back

---
+++ dist/a1atscript/ToAnnotation.js
@@ -0,0 +1,25 @@
+export default function ToAnnotation(AnnotationClass) {
+  var decorator = function(...callParams) {
+    callParams.unshift(null);
+    return function(targetClass) {
+      var oldAnnotation = Object.getOwnPropertyDescriptor(targetClass, 'annotations');
+      if (oldAnnotation) {
+        var oldGetter = oldAnnotation.get
+        Object.defineProperty(targetClass, 'annotations', {
+          configurable: true,
+          get: function() {
+            return oldGetter().concat([new (Function.prototype.bind.apply(AnnotationClass, callParams))]);
+          }});
+      } else {
+        Object.defineProperty(targetClass, 'annotations', {
+          configurable: true,
+          get: function() {
+          return [new (Function.prototype.bind.apply(AnnotationClass, callParams))];
+          }});
+      }
+      return targetClass;
+    };
+  };
+  decorator.originalClass = AnnotationClass;
+  return decorator;
+}
