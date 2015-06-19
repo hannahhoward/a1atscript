fdebbafa12094bb0593db00e8c6b7b7533d3fff4
Tim Kindberg <tim.kindberg@kroger.com>
2015-06-19 12:56:34 -0400

    Prevent usage of both attr and bind-attr on a component's element

--- test/PropertiesBuilder_spec.js
+++ test/PropertiesBuilder_spec.js
@@ -19,16 +19,36 @@ describe("PropertiesBuilder", function() {
 
   it("should setup the correct properties", function() {
     expect(properties).toEqual({
-      "cheese": "@danish",
-      "___bindable___cheese": "=?bindDanish",
-      "apple": "@apple",
-      "___bindable___apple": "=?bindApple"
+      "_@_cheese": "@danish",
+      "_=_cheese": "=?bindDanish",
+      "_@_apple": "@apple",
+      "_=_apple": "=?bindApple"
     });
   });
 
-  it("should setup the setters", function() {
+  it("should setup the bind setter", function() {
     var component = new Component();
-    component.___bindable___apple = "value";
-    expect(component.apple).toEqual("value");
+    component["_=_apple"] = "bind";
+    expect(component.apple).toEqual("bind");
+  });
+
+  it("should setup the string setter", function() {
+    var component = new Component();
+    component["_@_apple"] = "string";
+    expect(component.apple).toEqual("string");
+  });
+
+  it("should disable string if bind is used first", function() {
+    var component = new Component();
+    component["_=_apple"] = "bind";
+    expect(() => component["_@_apple"] = "string").toThrow(new Error('Cannot use bind-apple and apple simultaneously'));
+    expect(component.apple).toEqual('bind');
+  });
+
+  it("should disable bind if string is used first", function() {
+    var component = new Component();
+    component["_@_apple"] = "string";
+    expect(() => component["_=_apple"] = "bind").toThrow(new Error('Cannot use bind-apple and apple simultaneously'));
+    expect(component.apple).toEqual('string');
   });
 });
